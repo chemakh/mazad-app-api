3e2f8484e23574458a3a28bff5495fe00db49724
chemakh <lazher.chemakh@gmail.com>
2017-11-24 14:05:35 +0100

    enable activation process

--- mazad_service/src/main/java/ch/com/mazad/service/UserService.java
+++ mazad_service/src/main/java/ch/com/mazad/service/UserService.java
@@ -27,63 +27,57 @@ import java.util.stream.Collectors;
 /**
  * Created by Chemakh on 22/06/2017.
  */
-
 @Service
-public class UserService {
-
+public class UserService
+{
     private static Logger logger = LoggerFactory.getLogger(UserService.class);
-
     @Inject
     private UserRepository userRepository;
-
     @Inject
     private PasswordEncoder passwordEncoder;
-
     @Inject
     private PhotoService photoService;
-
     @Inject
     private MailService mailService;
-
     @Inject
     private DTOMapper mapper;
 
+    public UserDTO createUser(UserDTO userDTO, MultipartFile avatar) throws MazadException, IOException
+    {
 
-    public UserDTO createUser(UserDTO userDTO, MultipartFile avatar) throws MazadException, IOException {
-    	
-    	if(userDTO.getPassword() == null)
-    		throw MazadException.validationErrorBuilder(new FieldErrorDTO("User","password", "must_be_not_null"));
+        if(userDTO.getPassword() == null)
+            throw MazadException.validationErrorBuilder(new FieldErrorDTO("User", "password", "must_be_not_null"));
 
         checkIfEmailIsUsed(userDTO.getEmail(), userDTO.getReference());
 
         userDTO.setReference(TokenUtil.generateReference());
 
-        if (userDTO.getAddress() != null)
+        if(userDTO.getAddress() != null)
             userDTO.getAddress().setReference(TokenUtil.generateReference());
 
         String encryptedPassword = passwordEncoder.encode(userDTO.getPassword());
         userDTO.setPassword(encryptedPassword);
         userDTO.setReference(TokenUtil.generateReference());
 
-        if (avatar != null)
+        if(avatar != null)
             userDTO.setAvatar(mapper.fromPhotoToDTO(photoService.createPhoto(avatar, null)));
 
         User user = mapper.fromDTOToUser(userDTO);
         user.setEmailKey(TokenUtil.generateCode());
-        user.setActivated(true);
         user = userRepository.save(user);
 
-        // mailService.sendActivationEmail(userDTO, user.getEmailKey());
+        mailService.sendActivationEmail(userDTO, user.getEmailKey());
 
         return mapper.fromUserToDTO(user);
     }
 
-    public UserDTO updateUser(UserDTO userDTO, String reference) throws MazadException {
+    public UserDTO updateUser(UserDTO userDTO, String reference) throws MazadException
+    {
 
         User user = userRepository.findOneByReference(reference)
                 .orElseThrow(() -> MazadException.resourceNotFoundExceptionBuilder(User.class, userDTO.getReference()));
 
-        if (userDTO.getAddress() == null)
+        if(userDTO.getAddress() == null)
             userDTO.setAddress(mapper.fromAddressToDTO(user.getAddress()));
         mapper.updateUserFromDto(userDTO, user);
         Optional.ofNullable(userDTO.getPassword()).ifPresent(pass -> user.setPassword(passwordEncoder.encode(pass)));
@@ -91,18 +85,23 @@ public class UserService {
         return mapper.fromUserToDTO(userRepository.save(user));
     }
 
-    public UserDTO updateUserAvatar(String userRef, MultipartFile photo) throws IOException, MazadException {
+    public UserDTO updateUserAvatar(String userRef, MultipartFile photo) throws IOException, MazadException
+    {
         return photoService.createUserAvatar(photo, userRef);
     }
 
-    public List<UserDTO> getUser(String reference) throws MazadException {
+    public List<UserDTO> getUser(String reference) throws MazadException
+    {
 
-        if (reference != null)
+        if(reference != null)
 
-            try {
+            try
+            {
                 return Collections.singletonList(userRepository.findOneByReference(reference).map(mapper::fromUserToDTO)
                         .orElseThrow(() -> MazadException.resourceNotFoundExceptionBuilder(User.class, reference)));
-            } catch (MazadException e) {
+            }
+            catch (MazadException e)
+            {
                 return Collections.singletonList(userRepository.findOneByReference(reference).map(mapper::fromUserToDTO)
                         .orElseThrow(() -> MazadException.resourceNotFoundExceptionBuilder(User.class, reference)));
             }
@@ -111,7 +110,8 @@ public class UserService {
                     .collect(Collectors.toList());
     }
 
-    public JSONObject deleteUser(String reference) throws MazadException {
+    public JSONObject deleteUser(String reference) throws MazadException
+    {
 
         User user = userRepository.findOneByReference(reference)
                 .orElseThrow(() -> MazadException.resourceNotFoundExceptionBuilder(User.class, reference));
@@ -126,19 +126,20 @@ public class UserService {
         return result;
     }
 
-
-    public UserDTO getCurrentUser(Principal principal) {
+    public UserDTO getCurrentUser(Principal principal)
+    {
         logger.info("Getting current user");
 
-        if (principal instanceof User) {
-            User user = (User) principal;
+        if(principal instanceof User)
+        {
+            User user = (User)principal;
             return mapper.fromUserToDTO(user);
-
         }
         return null;
     }
 
-    public UserDTO activateRegistration(String key) throws MazadException {
+    public UserDTO activateRegistration(String key) throws MazadException
+    {
 
         logger.debug("Activating user for activation key {}", key);
 
@@ -158,7 +159,8 @@ public class UserService {
                 }).orElseThrow(() -> MazadException.invalidCodeExceptionBuilder("Email Key"));
     }
 
-    public UserDTO requestResetPassword(String mail) throws MazadException {
+    public UserDTO requestResetPassword(String mail) throws MazadException
+    {
         return userRepository.findOneByEmail(mail)
                 .filter(User::isActivated)
                 .map(user ->
@@ -172,7 +174,8 @@ public class UserService {
                 }).orElseThrow(() -> MazadException.resourceNotFoundExceptionBuilder(User.class, mail, MazadException.WITH_EMAIL));
     }
 
-    public UserDTO completePasswordReset(String newPassword, String key) throws MazadException {
+    public UserDTO completePasswordReset(String newPassword, String key) throws MazadException
+    {
         logger.debug("Reset user password for reset key {}", key);
 
         return userRepository.findOneByResetPasswordKey(key)
@@ -187,41 +190,45 @@ public class UserService {
                 .orElseThrow(() -> MazadException.invalidCodeExceptionBuilder("Password Key"));
     }
 
-    public boolean changePassword(String oldPassword, String newPassword) throws MazadException {
+    public boolean changePassword(String oldPassword, String newPassword) throws MazadException
+    {
         logger.debug("Reset user password for reset");
 
         User user = getCurrentUser();
-        if (passwordEncoder.matches(oldPassword, user.getPassword())) {
+        if(passwordEncoder.matches(oldPassword, user.getPassword()))
+        {
             user.setPassword(passwordEncoder.encode(newPassword));
             userRepository.save(user);
-        } else {
+        }
+        else
+        {
             throw MazadException.validationErrorBuilder(new FieldErrorDTO("User", "Password", "must_match"));
         }
 
         return true;
-
     }
 
-    public boolean requestEmailCode() throws MazadException {
+    public boolean requestEmailCode() throws MazadException
+    {
         logger.debug("Reset user email ");
         User user = getCurrentUser();
         user.setEmailKey(TokenUtil.generateCode());
         mailService.sendActivationEmail(mapper.fromUserToDTO(userRepository.save(user)), user.getEmailKey());
         return true;
-
     }
 
-    public boolean requestEmailCode(String email) throws MazadException {
+    public boolean requestEmailCode(String email) throws MazadException
+    {
         logger.debug("Reset user email ");
         User user = userRepository.findOneByEmail(email).
                 orElseThrow(() -> MazadException.resourceNotFoundExceptionBuilder(User.class, SecurityUtils.getCurrentUserLogin(), MazadException.WITH_EMAIL));
         user.setEmailKey(TokenUtil.generateCode());
         mailService.sendActivationEmail(mapper.fromUserToDTO(userRepository.save(user)), user.getEmailKey());
         return true;
-
     }
 
-    public UserDTO verifyEmail(String code) throws MazadException {
+    public UserDTO verifyEmail(String code) throws MazadException
+    {
         logger.debug("Verify user email with  key {}", code);
         User currentUser = getCurrentUser();
         return userRepository.findOneByEmailKey(code)
@@ -237,44 +244,48 @@ public class UserService {
                 .orElseThrow(() -> MazadException.invalidCodeExceptionBuilder("Email Key"));
     }
 
-
-    public UserDTO getUserDetails(String reference) throws MazadException {
+    public UserDTO getUserDetails(String reference) throws MazadException
+    {
         User us = userRepository.findOneByReference(reference).
                 orElseThrow(() -> MazadException.resourceNotFoundExceptionBuilder(User.class, reference));
         return mapper.fromUserToDTO(us);
     }
 
-    public UserDTO getLoggedUser() throws MazadException {
+    public UserDTO getLoggedUser() throws MazadException
+    {
 
-        if (!SecurityUtils.checkIfThereIsUserLogged())
+        if(!SecurityUtils.checkIfThereIsUserLogged())
             throw MazadException.actionUnauthorizedErrorBuilder();
 
         return userRepository.findOneByEmail(SecurityUtils.getCurrentUserLogin()).map(mapper::fromUserToDTO).
                 orElseThrow(() -> MazadException.resourceNotFoundExceptionBuilder(User.class, SecurityUtils.getCurrentUserLogin(), MazadException.WITH_EMAIL));
     }
 
-    public User getCurrentUser() throws MazadException {
+    public User getCurrentUser() throws MazadException
+    {
 
-        if (!SecurityUtils.checkIfThereIsUserLogged())
+        if(!SecurityUtils.checkIfThereIsUserLogged())
             throw MazadException.actionUnauthorizedErrorBuilder();
 
         return userRepository.findOneByEmail(SecurityUtils.getCurrentUserLogin()).
                 orElseThrow(() -> MazadException.resourceNotFoundExceptionBuilder(User.class, SecurityUtils.getCurrentUserLogin(), MazadException.WITH_EMAIL));
     }
 
-    public void checkIfEmailIsUsed(String email, String reference) throws MazadException {
-        if (email != null && !email.isEmpty()) {
+    public void checkIfEmailIsUsed(String email, String reference) throws MazadException
+    {
+        if(email != null && !email.isEmpty())
+        {
             Optional<String> find = userRepository.getUserReferenceByEmail(email).
                     filter(usRef -> reference == null || reference.isEmpty() || !usRef.equals(reference));
-            if (find.isPresent()) {
+            if(find.isPresent())
+            {
                 throw MazadException.identifierAlreadyInUseExceptionBuilderBuilder("Email", email, new FieldErrorDTO("User", "Email", "already_in_use"));
             }
-
         }
-
     }
 
-    public UserDTO activateRegistration(String email, String key) throws MazadException {
+    public UserDTO activateRegistration(String email, String key) throws MazadException
+    {
         logger.debug("Activating user for activation key {}", key);
 
         return userRepository.findOneByEmail(email)
